f99fce1a59debea3d67dbd3aef20942319a5778a
yudzofc <106936091+yudzofc@users.noreply.github.com>
2022-09-10 20:04:32 +0700

    -

--- config.js
+++ config.js
@@ -5,72 +5,72 @@
 // RECODE BY ZIFABOTZ
 
 //=============『 Utama 』================== //
-global.owner = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
-global.mods = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
-global.prems = ['6281251549899'] //ganti pakai nomormu biar bot berfungsi
+global.owner = ['6285693650294','6289652131288','6285600380823'] //ganti pakai nomormu biar bot berfungsi
+global.mods = ['6285693650294'] //ganti pakai nomormu biar bot berfungsi
+global.prems = ['6285693650294','6289652131288','6285600380823'] //ganti pakai nomormu biar bot berfungsi
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ᴹᴿ᭄ DaniBotzOffcོ_1ঔৣ꧂'
-global.numberowner = '6281251549899' //ganti pakai nomor mu
-global.instagram = 'https://www.instagram.com/danibotz_official' //ganti pakai Instagram mu
-global.github = 'https://github.com/YTDANIBOTZ' //ganti aja pakai githubmu
-global.tt = '"_"' //ganti aja pakai tiktok mu
-global.sc = 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg' //jngn ganti hargai creator
-global.ss = 'https://youtu.be/3m7tRD4RklM' //jngn ganti hargai creator
-global.sss = 'https://youtu.be/PzUH1ApMCig' //jngn ganti hargai creator
-global.snh = 'https://nhentai.net/g/365296/'
-global.yt = 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg'
-global.dana = '081251549899' //ganti pakai nomor dana mu
-global.pulsa = '081352848936' //ganti pakai nomor pulsa mu
-global.gopay = '081352848936' //ganti pakai nomor Gopay mu
+global.nameowner = 'ᴹᴿ᭄ YudsOfcོ_ঔৣ꧂'
+global.numberowner = '6285693650294' //ganti pakai nomor mu
+global.instagram = 'https://www.instagram.com/lebihkerendariandy_warholl.id' //ganti pakai Instagram mu
+global.github = 'https://github.com/yudzofc' //ganti aja pakai githubmu
+global.tt = '"https://www.tiktok.com/@siapaaku093_"' //ganti aja pakai tiktok mu
+global.sc = 'https://github.com/yudzofc' //jngn ganti hargai creator
+global.ss = 'https://s.id/WahyudiID' //jngn ganti hargai creator
+global.sss = 'https://link.bio/wahyup094.repl.co' //jngn ganti hargai creator
+global.snh = 'https://t.me/@LebihkerendariAndy_Warholl'
+global.yt = 'https://youtube.com/channel/UC8Ukf0gpRRYmpkF74v9V8Yw'
+global.dana = '085693650294' //ganti pakai nomor dana mu
+global.pulsa = '085693650294' //ganti pakai nomor pulsa mu
+global.gopay = '085811571839' //ganti pakai nomor Gopay mu
 
 //=============『 Info Bot 』=================//
-global.namebot = '❋ཻུ۪۪⸙Danibotz-Ofc⳹ ❋ཻུ۪۪' //ganti jadiin nama bot mu
-global.gc = 'https://chat.whatsapp.com/BiJs5Xl4oJA1Ama5BahUwd' //ganti pakai grup mu
-global.web = 'https://youtube.com/channel/UCoHJ9D9Sy3TLp0mUHBrj1cg' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
-global.webm = 'https://www.instagram.com/danibotz_official' //ganti pakai Instagram mu
-global.saweria = '"_"' //bebas ganti aja pakai apa
-global.nomer = 'https://wa.me/6281251549899' //ganti jadi nomor mu
+global.namebot = '❋ཻུ۪۪⸙YudsBotz-Md⳹ ❋ཻུ۪۪' //ganti jadiin nama bot mu
+global.gc = 'https://chat.whatsapp.com/DnOeCnfvCvREjasisjOCpU' //ganti pakai grup mu
+global.web = 'https://s.id/WahyudiID' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.webm = 'https://link.bio/wahyup094.repl.co' //ganti pakai Instagram mu
+global.saweria = '"https://saweria.co/WahyudiiDpedonh"' //bebas ganti aja pakai apa
+global.nomer = 'https://wa.me/6285693650294' //ganti jadi nomor mu
 global.price1 = '𝐍𝐈𝐇 𝐁𝐀𝐍𝐆 𝐋𝐈𝐒𝐓 𝐒𝐄𝐖𝐀𝐍𝐘𝐀\n\n》PERMANEN Rp 10.000\nMAKSIMAL 2 GRUP\n》PERMANEN  Rp 20.000\nMAKSIMAL 6 GRUP \n》1 BULAN Rp 5.000\nMAKSIMAL 1 GRUP'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ini juga ganti pakai foto bot mu
-global.fotonya3 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ini juga ganti
-global.fotonya4 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ini juga ganti
-global.fotonya5 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ini juga ganti
-global.fotonya6 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ini ganti pakai foto sosialmedia mu
-global.fotonya7 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //ini juga ganti
-global.fotonya8 = 'https://telegra.ph/file/2f4105651a1d9aba5a3fc.jpg' //foto session
-global.donasi = 'https://telegra.ph/file/4720d8dc811569a29e48f.jpg' //ganti pakai poto payment atau apa
+global.fotonya1 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ini juga ganti pakai foto bot mu
+global.fotonya3 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ini juga ganti
+global.fotonya4 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ini juga ganti
+global.fotonya5 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ini juga ganti
+global.fotonya6 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ini ganti pakai foto sosialmedia mu
+global.fotonya7 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ini juga ganti
+global.fotonya8 = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //foto session
+global.donasi = 'https://telegra.ph/file/e4964165f0bc424a54815.png' //ganti pakai poto payment atau apa
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '❋ཻུ۪۪Danibotz-Ofc⳹ ❋ཻུ۪۪'
+global.wm = '❋ཻུ۪۪YudsBotz-Md⳹ ❋ཻུ۪۪'
 global.watermark = wm
-global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜDani_ofcོ1꧅'
-global.wm3 = 'ᯤ Dani Offcial'
-global.titlebot = 'ᗪani Offcial ✯✯✯✯✯'
+global.wm2 = 'ᯤ creator by Oᵂηʀ᭄๖ۣۣۜYudsOfcོ꧅'
+global.wm3 = 'ᯤ Yuds Offcial'
+global.titlebot = 'Yudi Offcial ✯✯✯✯✯'
 global.htki = '––––––『' 
 global.htka = '』––––––'
-global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
+global.media = 'https://telegra.ph/file/e4964165f0bc424a54815.png'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wait = '⏳Tunggu kurang lebih 1 menit karna sedang diproses. . .'
-global.eror = '_*🚨 Server Bot Sedang Error*_'
+global.wait = '⏳ *Lagi Loading Nih Sabar Ya.....   Orang Sabar Disayang Janda 😊😊*'
+global.eror = '🚨 *Terjadi Kesalahan Server. Harap Coba Beberapa Saat Lagi*'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '⏳ Stiker sedang Proses'
-global.packname = 'DaniBotz-Ofc'
-global.author = '@Dani Official'
-global.bg = 'await(await fetch(https://telegra.ph/file/4a7e5f18efaadec18a7a0.jpg)).buffer()'
+global.packname = 'YudsBotz-Md'
+global.author = 'WahyudiID'
+global.bg = 'await(await fetch(https://telegra.ph/file/e4964165f0bc424a54815.png)).buffer()'
 
 
 //============== LOGO ==============//
 global.thumbnailUrl = [
-  'https://telegra.ph/file/81260a8b9e8cff26d2b48.jpg', 'https://telegra.ph/file/ac4928f0824a2a0492737.jpg',
-  'https://telegra.ph/file/6359b013bc7e52c3b346f.jpg', 'https://telegra.ph/file/d43c89a5d2da72875ec05.jpg',
-  'https://telegra.ph/file/7d6c0e35f9c8f52715541.jpg', 'https://telegra.ph/file/ef4b742d47e6a9115e2ff.jpg',
-  'https://telegra.ph/file/55e5af5f33fbd57104187.jpg', 'https://telegra.ph/file/af236598456b95884bd15.jpg',
-  'https://telegra.ph/file/de92ed4a729887ffc974c.jpg', 'https://telegra.ph/file/00ce42a193b1dbbf907d4.jpg'
+  'https://telegra.ph/file/e4964165f0bc424a54815.png', 'https://telegra.ph/file/e4964165f0bc424a54815.png',
+  'https://telegra.ph/file/e4964165f0bc424a54815.png', 'https://telegra.ph/file/e4964165f0bc424a54815.png',
+  'https://telegra.ph/file/e4964165f0bc424a54815.png', 'https://telegra.ph/file/e4964165f0bc424a54815.png',
+  'https://telegra.ph/file/e4964165f0bc424a54815.png', 'https://telegra.ph/file/e4964165f0bc424a54815.png',
+  'https://telegra.ph/file/e4964165f0bc424a54815.png', 'https://telegra.ph/file/e4964165f0bc424a54815.png'
 ]
 
 //=========== TYPE DOCUMENT ===========//
@@ -80,7 +80,7 @@ global.dxlsx = 'application/vnd.openxmlformats-officedocument.spreadsheetml.shee
 global.dpdf = 'application/pdf'
 global.drtf = 'text/rtf'
 
-global.thumbdoc = 'https://telegra.ph/file/6e45318d7c76f57e4a8bd.jpg'
+global.thumbdoc = 'https://telegra.ph/file/e4964165f0bc424a54815.png'
 
 //=========== FAKE SIZE ===========//
 global.fsizedoc = '99999999999999' // default 10TB
